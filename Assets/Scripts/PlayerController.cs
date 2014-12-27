25b92acf6deeb6f5dfe5b4e344635ac0449a209e
Omri Keret <omri.keret@gmail.com>
2014-12-27 13:59:16 +0200

    added the menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -74,6 +74,7 @@ public class PlayerController : MonoBehaviour {
 		{
 			Dash();
 		}
+		leftScreen ();
 	}
 
 
@@ -124,5 +125,26 @@ public class PlayerController : MonoBehaviour {
 			Flip ();
 	}
 
+	private void leftScreen(){
+		var dist = (transform.position - Camera.main.transform.position).z;
+		var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0,dist)).x; 
+		var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1,0,dist)).x;
+		var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,1,dist)).y; 
+		var buttomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0,dist)).y; 
+		// teleport me to the other side of the screen when I reach the edge
+		if (transform.position.x > rightBorder) {
+			transform.position = new Vector3 (leftBorder, transform.position.y, 0);
+		}
+		if (transform.position.x < leftBorder) {
+			transform.position = new Vector3 (rightBorder, transform.position.y, 0);
+		}
+		// teleport me to the top or buttom
+		if (transform.position.y > topBorder) {
+			transform.position = new Vector3 (transform.position.x,buttomBorder, 0);
+		}
+		if (transform.position.y < buttomBorder) {
+			transform.position = new Vector3 (transform.position.x, topBorder, 0);
+		}
+	}
 
 }
